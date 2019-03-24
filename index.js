763d23ec26733c5ee2e72e17bdff9850dfe2657d
Eric Meldrum <eric.meldrum@gmail.com>
2019-03-24 15:18:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,19 +5,13 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function sayHiToGrandma(string) {
-  return string.toLowerCase()
-}
-
-var uppercase = "HELLO!"
-var lowercase = "hello"
-var love = "I love you, Grandma"
- 
-if (string.toUpperCase() === string) {
+  if (string.toUpperCase() === string) {
   console.log("YES INDEED!")
 } else if (string.toLowerCase() === string) {
   console.log("I can't hear you!")
 } else if (string === "I love you, Grandma.") {
   console.log("I love you too.")
+}
 }
 
 sayHiToGrandma('HELLO!')
