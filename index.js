fe1668629b733da4a8bc5a3cca836d8d1bba4e97
Eric Meldrum <eric.meldrum@gmail.com>
2019-03-24 15:22:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+function logShout(string) {
+  console.log()
+}
 function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
   console.log("YES INDEED!")
